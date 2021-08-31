9cae22810480cacc2ae04dfc3cee7199086762e6
Michael Wettstein <chischte@gmx.ch>
2021-08-31 17:30:14 +0200

    List com port in windows

--- src/data_logger/raspberry_log_merger/raspberry_log_merger.py
+++ src/data_logger/raspberry_log_merger/raspberry_log_merger.py
@@ -2,3 +2,18 @@
 # GET DATA FROM ARDUINO_CURRENT_LOGGER
 # MERGE DATA TO LOG
 # PUSH LOG TO FIREBASE
+
+# Show list of serial devices:
+import os
+import sys
+import time
+import serial
+import serial.tools.list_ports
+
+print('Search...')
+ports = serial.tools.list_ports.comports(include_links=False)
+for port in ports :
+    print('Find port '+ port.device)
+
+    
+
