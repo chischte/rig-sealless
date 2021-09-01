64ebd6e5618b4a0925328c33f73eecf1d098f28a
Michael Wettstein <chischte@gmx.ch>
2021-09-01 14:43:29 +0200

    Filter and split logs

--- src/raspberry_log_merger/raspberry_log_merger.py
+++ src/raspberry_log_merger/raspberry_log_merger.py
@@ -12,24 +12,58 @@ import serial.tools.list_ports
 
 print('Search...')
 ports = serial.tools.list_ports.comports(include_links=False)
-for port in ports :
-    print('Find port '+ port.device)
+for port in ports:
+    print('Find port ' + port.device)
 
-ser = serial.Serial(
-    port='COM3',\
-    baudrate=115200,\
-    parity=serial.PARITY_NONE,\
-    stopbits=serial.STOPBITS_ONE,\
-    bytesize=serial.EIGHTBITS,\
-        timeout=0)
+ser_controllino = serial.Serial(
+    port='COM3',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0)
 
-print("connected to: " + ser.portstr)
-count=1
+ser_arduino = serial.Serial(
+    port='COM5',
+    baudrate=115200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0)
+
+
+print("connected to: " + ser_controllino.portstr)
+print("connected to: " + ser_arduino.portstr)
+count = 1
+
+def verify_log(log):
+    if(log==""): #  remove empty reads
+        return False
+    return True
+
+def split_log(log):
+    
+    log=log.split(';')
+    print(log)
+
+    return log
+
 
 while True:
-    line = ser.readline()
-    print(line)
+    line_controllino = ser_controllino.readline().decode('utf-8')
+    #print(line_controllino)
+    
+    line_arduino = ser_arduino.readline().decode('utf-8')
+    #print(line_arduino)
+
+    if(verify_log(line_controllino)):
+        split_log(line_controllino)
+
+    if(verify_log(line_arduino)):
+        split_log(line_arduino)
+
+
     time.sleep(0.5)
 
-ser.close()
 
+ser.close()
