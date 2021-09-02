838f5d56c7aeef5936090b9746714a1686766d0d
Michael Wettstein <chischte@gmx.ch>
2021-09-02 11:27:57 +0200

    Convert log_merger to class

--- src/raspberry_log_merger/raspberry_log_merger.py
+++ src/raspberry_log_merger/raspberry_log_merger.py
@@ -1,5 +1,5 @@
+#!/usr/bin/env python3
 
-# Show list of serial devices:
 import os
 import sys
 import time
@@ -10,110 +10,118 @@ from log import log
 # Create log objects
 log_object = log()
 
-arduino = 0
-controllino = 0
 
+class rapberry_log_merger():
 
-def get_list_of_serial_devices():
-    # Search available ports:
-    print('Search...')
-    ports = serial.tools.list_ports.comports(include_links=False)
-    print('---------------------------------------------')
-    print('PORTSCAN RESULTS:')
-    for port in ports:
+    def __init__(self):
+        self.arduino = 0
+        self.controllino = 0
+        self.log_object = log()
+
+    def get_list_of_serial_devices(self):
+        # Search available ports:
+        print('Search...')
+        ports = serial.tools.list_ports.comports(include_links=False)
         print('---------------------------------------------')
-        print(port.device)
-        print(port.manufacturer)
-        print(port.hwid)
-        print(port.serial_number)  # Controllino: 85830303039351315291
-        print(port.description)  # Arduino Mega: USB-SERIAL CH340 (COM5)
-        print(port.vid)
-        print(port.pid)
-        print('---------------------------------------------')
+        print('PORTSCAN RESULTS:')
+        for port in ports:
+            print('---------------------------------------------')
+            print(port.device)
+            print(port.manufacturer)
+            print(port.hwid)
+            print(port.serial_number)  # Controllino: 85830303039351315291
+            print(port.description)  # Arduino Mega: USB-SERIAL CH340 (COM5)
+            print(port.vid)
+            print(port.pid)
+            print('---------------------------------------------')
 
-def connect_to_serial_devices():
-    global arduino
-    global controllino
+    def connect_to_serial_devices(self):
 
-    ports = serial.tools.list_ports.comports(include_links=False)
+        ports = serial.tools.list_ports.comports(include_links=False)
 
-    for port in ports:
-        if(port.vid == 9025) and (port.pid == 66):
-            arduino_port = port.device
-            print(f'ARDUINO PORT: {arduino_port}')
-        if(port.vid == 6790) and (port.pid == 29987):
-            controllino_port = port.device
-            print(f'CONTROLLINO PORT: {controllino_port}')
+        for port in ports:
+            if(port.vid == 9025) and (port.pid == 66):
+                arduino_port = port.device
+                print(f'ARDUINO PORT: {arduino_port}')
+            if(port.vid == 6790) and (port.pid == 29987):
+                controllino_port = port.device
+                print(f'CONTROLLINO PORT: {controllino_port}')
 
-    controllino = serial.Serial(
-        port=controllino_port,
-        baudrate=115200,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=0)
-    print("connected to: " + controllino.portstr)
+        self.controllino = serial.Serial(
+            port=controllino_port,
+            baudrate=115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=0)
+        print("connected to: " + self.controllino.portstr)
 
-    arduino = serial.Serial(
-        port=arduino_port,
-        baudrate=115200,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=0)
-    print("connected to: " + arduino.portstr)
+        self.arduino = serial.Serial(
+            port=arduino_port,
+            baudrate=115200,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=0)
+        print("connected to: " + self.arduino.portstr)
 
-def process_serial_read(readline):
-    readline = readline.decode('utf-8')
-    if(readline == ""):
+    def process_serial_read(self, readline):
+        readline = readline.decode('utf-8')
+        if(readline == ""):
+            return
+        readline = readline.split(';')
+        if(readline[0] == 'LOG'):
+            self.add_info_to_log(readline)
+
+    def upload_log(self, log_object):
         return
-    readline = readline.split(';')
-    if(readline[0] == 'LOG'):
-        add_info_to_log(readline)
 
-def upload_log(log_object):
-    return
+    def set_log_completed(self):
+        self.log_object.print_log()
+        self.upload_log(log_object)
+        self.log_object.reset_log()
 
-def set_log_completed():
-    log_object.print_log()
-    upload_log(log_object)
-    log_object.reset_log()
+    def add_info_to_log(self, readline):
 
-def add_info_to_log(readline):
+        if readline[1] == 'CYCLE_TOTAL':
+            self.set_log_completed()
+            self.log_object.cycle_total = readline[2]
 
-    if readline[1] == 'CYCLE_TOTAL':
-        set_log_completed()
-        log_object.cycle_total = readline[2]
+        if readline[1] == 'CYCLE_RESET':
+            self.log_object.cycle_reset = readline[2]
 
-    if readline[1] == 'CYCLE_RESET':
-        log_object.cycle_reset = readline[2]
+        if readline[1] == 'FORCE':
+            self.log_object.force = readline[2]
 
-    if readline[1] == 'FORCE':
-        log_object.force = readline[2]
+        if readline[1] == 'START_TENSION':
+            self.log_object.set_tool_is_tensioning()
 
-    if readline[1] == 'START_TENSION':
-        log_object.set_tool_is_tensioning()
+        if readline[1] == 'START_CRIMP':
+            self.log_object.set_tool_is_crimping()
 
-    if readline[1] == 'START_CRIMP':
-        log_object.set_tool_is_crimping()
+        if readline[1] == 'CURRENT_MAX':
+            if(log_object.tool_is_tensioning):
+                self.log_object.current_tension = readline[2]
+            if(log_object.tool_is_crimping):
+                self.log_object.crimp_current = readline[2]
 
-    if readline[1] == 'CURRENT_MAX':
-        if(log_object.tool_is_tensioning):
-            log_object.current_tension = readline[2]
-        if(log_object.tool_is_crimping):
-            log_object.crimp_current = readline[2]
+    def read_serial_ports(self):
+        log_merger.process_serial_read(self.controllino.readline())
+        log_merger.process_serial_read(self.arduino.readline())
 
 
-get_list_of_serial_devices()
-connect_to_serial_devices()
+if __name__ == '__main__':
 
-while True:
+    log_merger = rapberry_log_merger()
 
-    # Read and process serial inputs:
-    process_serial_read(controllino.readline())
-    process_serial_read(arduino.readline())
+    log_merger.get_list_of_serial_devices()
+    log_merger.connect_to_serial_devices()
 
-    time.sleep(0.5)
+    while True:
 
+        # Read and process serial inputs:
+        log_merger.read_serial_ports()
 
-ser.close()
+        time.sleep(0.5)
+
+    ser.close()
