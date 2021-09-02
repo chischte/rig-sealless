3417faab7faa157574342acf71b3d6aed426c5c9
Michael Wettstein <chischte@gmx.ch>
2021-09-02 17:31:24 +0200

    Make log_manager catch errors

--- src/log_manager/raspberry_log_mananager.py
+++ src/log_manager/raspberry_log_mananager.py
@@ -8,6 +8,7 @@ import serial.tools.list_ports
 from log_object import log_object
 from firebase_helper import firebase_helper
 
+
 class raspberry_log_manager():
 
     def __init__(self):
@@ -17,51 +18,59 @@ class raspberry_log_manager():
         self.firebase_helper = firebase_helper()
 
     def get_list_of_serial_devices(self):
-        # Search available ports:
-        print('Search...')
-        ports = serial.tools.list_ports.comports(include_links=False)
-        print('---------------------------------------------')
-        print('PORTSCAN RESULTS:')
-        for port in ports:
+        try:
+            # Search available ports:
+            print('Search...')
+            ports = serial.tools.list_ports.comports(include_links=False)
             print('---------------------------------------------')
-            print(port.device)
-            print(port.manufacturer)
-            print(port.hwid)
-            print(port.serial_number)  # Controllino: 85830303039351315291
-            print(port.description)  # Arduino Mega: USB-SERIAL CH340 (COM5)
-            print(port.vid)
-            print(port.pid)
-            print('---------------------------------------------')
+            print('PORTSCAN RESULTS:')
+            for port in ports:
+                print('---------------------------------------------')
+                print(port.device)
+                print(port.manufacturer)
+                print(port.hwid)
+                print(port.serial_number)  # Controllino: 85830303039351315291
+                print(port.description)  # Arduino Mega: USB-SERIAL CH340 (COM5)
+                print(port.vid)
+                print(port.pid)
+                print('---------------------------------------------')
+        except Exception as error:
+            error_message = 'CAUGHT AN ERROR WHILE SEARCHING PORTS !!!'
+            print(error_message, error)
 
     def connect_to_serial_devices(self):
 
-        ports = serial.tools.list_ports.comports(include_links=False)
+        try:
+            ports = serial.tools.list_ports.comports(include_links=False)
 
-        for port in ports:
-            if(port.vid == 9025) and (port.pid == 66):
-                arduino_port = port.device
-                print(f'ARDUINO PORT: {arduino_port}')
-            if(port.vid == 6790) and (port.pid == 29987):
-                controllino_port = port.device
-                print(f'CONTROLLINO PORT: {controllino_port}')
+            for port in ports:
+                if(port.vid == 9025) and (port.pid == 66):
+                    arduino_port = port.device
+                    print(f'ARDUINO PORT: {arduino_port}')
+                if(port.vid == 6790) and (port.pid == 29987):
+                    controllino_port = port.device
+                    print(f'CONTROLLINO PORT: {controllino_port}')
 
-        self.controllino = serial.Serial(
-            port=controllino_port,
-            baudrate=115200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=0)
-        print("connected to: " + self.controllino.portstr)
+            self.controllino = serial.Serial(
+                port=controllino_port,
+                baudrate=115200,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                timeout=0)
+            print("connected to: " + self.controllino.portstr)
 
-        self.arduino = serial.Serial(
-            port=arduino_port,
-            baudrate=115200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            timeout=0)
-        print("connected to: " + self.arduino.portstr)
+            self.arduino = serial.Serial(
+                port=arduino_port,
+                baudrate=115200,
+                parity=serial.PARITY_NONE,
+                stopbits=serial.STOPBITS_ONE,
+                bytesize=serial.EIGHTBITS,
+                timeout=0)
+            print("connected to: " + self.arduino.portstr)
+        except Exception as error:
+            error_message = 'CAUGHT AN ERROR WHILE TRYING TO CONNECT TO PORTS !!!'
+            print(error_message, error)
 
     def process_serial_read(self, readline):
         readline = readline.decode('utf-8')
@@ -72,15 +81,15 @@ class raspberry_log_manager():
             self.add_info_to_log(readline)
 
     def upload_log(self):
-        data=self.log_object.get_db_string()
+        data = self.log_object.get_db_string()
         self.firebase_helper.push(data)
 
     def set_log_completed(self):
         self.log_object.print_log()
         # Do not upload empty log at startup:
-        if(self.log_object.cycle_total!=0):
+        if(self.log_object.cycle_total != 0):
             self.upload_log()
-            
+
         self.log_object.reset_log()
 
     def add_info_to_log(self, readline):
@@ -121,8 +130,16 @@ if __name__ == '__main__':
 
     while True:
 
-        # Read and process serial inputs:
-        log_manager.read_serial_ports()
+        try:
+            # Read and process serial inputs:
+            log_manager.read_serial_ports()
+        except Exception as error:
+            error_message = 'CAUGHT AN ERROR IN THE MAIN LOOP !!!'
+            print(error_message, error)
+            log_manager.get_list_of_serial_devices()
+            log_manager.connect_to_serial_devices()
+            time.sleep(3)
+
 
         time.sleep(0.5)
 
