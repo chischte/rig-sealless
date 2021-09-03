50dfbd4298c892f4a377f08c5895790915e00921
Michael Wettstein <chischte@gmx.ch>
2021-09-03 13:46:03 +0200

    Make email_helper a class

--- src/log_manager/email_helper.py
+++ src/log_manager/email_helper.py
@@ -1,20 +1,44 @@
+from email import message
 import smtplib
 import ssl
 from email.mime.text import MIMEText
 from email_secret_config import *
+from datetime import datetime
 
-sender = 'dauertest.bst@gmail.com'
-receivers = ['m.wettstein@signode.com']
-body_of_email = '\
-<h1> DAUERTEST HAT GESTOPPT <h1>\
-'
+class email_helper():
 
-msg = MIMEText(body_of_email, 'html')
-msg['Subject'] = 'DAUERTEST HAT GESTOPPT'
-msg['From'] = sender
-msg['To'] = ','.join(receivers)
+    def __init__(self):
+        self.sender = 'dauertest.bst@gmail.com'
+        self.receivers = ['m.wettstein@signode.com']
 
-s = smtplib.SMTP_SSL(host='smtp.gmail.com', port=465)
-s.login(user='dauertest.bst', password=email_password)
-s.sendmail(sender, receivers, msg.as_string())
-s.quit()
+    def send_mail(self, subject, message_body):
+        msg = MIMEText(message_body, 'html')
+        msg['Subject'] = subject
+        msg['From'] = self.sender
+        msg['To'] = ','.join(self.receivers)
+        s = smtplib.SMTP_SSL(host='smtp.gmail.com', port=465)
+        s.login(user='dauertest.bst', password=email_password)
+        s.sendmail(self.sender, self.receivers, msg.as_string())
+        s.quit()
+
+    def send_message_machine_error(self):
+        datestamp = datetime.now().strftime("%d/%m/%Y")
+        timestamp = datetime.now().strftime("%H:%M")
+        subject = f'BST DAUERTEST HAT GESTOPPT'
+        message_body = f'\
+        <h2> BST DAUERTEST HAT GESTOPPT AM {datestamp} UM {timestamp} UHR\
+        <h3>Obwohl der Dauertest im Automatikbetrieb lief, wurde während 60 Sekunden\
+        <br>kein Tool-Zyklus mehr erfolgreich durchgeführt.\
+        <p>Mögliche Ursachen:\
+            <ul>\
+            <li>Band leer\
+            <li>Störung am Gerät\
+            <li>Störung Dauertestrig\
+            <li>Störung Steuerung\
+            '
+        self.send_mail(subject, message_body)
+
+if __name__ == '__main__':
+    
+    email_helper = email_helper()
+    email_helper.send_message_machine_error()
