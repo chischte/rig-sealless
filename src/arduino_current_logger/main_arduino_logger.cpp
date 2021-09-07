d161108040901a783399c38a7e9f88a35290c620
Michael Wettstein <chischte@gmx.ch>
2021-09-07 10:43:16 +0200

    Change MCU from mega to uno

--- src/arduino_current_logger/main_arduino_logger.cpp
+++ src/arduino_current_logger/main_arduino_logger.cpp
@@ -2,13 +2,14 @@
 
 void setup() {
   Serial.begin(115200);
-  Serial1.begin(115200);
   Serial.println("EXIT SETUP"); // [A]
-  Serial1.println("EXIT SETUP SERIAL 1"); // [A]
 }
 
 void loop() {
-  Serial.println("LOG;CURRENT_MAX;34.52;"); // [A]
-  Serial1.println("Serial1"); // [A]
-  delay(1500); // wait for a second
+  float current = random(2000, 5400)/100.0;
+  String prefix = "LOG;CURRENT_MAX;";
+  String suffix = ";";
+  Serial.println(prefix + current + suffix); // [A]
+
+  delay(500); // wait for a second
 }
