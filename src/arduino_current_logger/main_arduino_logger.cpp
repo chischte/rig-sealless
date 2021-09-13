1e11306812d8c037a7d44e1dfb2e8f15bb2287bc
Michael Wettstein <chischte@gmx.ch>
2021-09-13 10:59:53 +0200

    Work on current logger

--- src/arduino_current_logger/main_arduino_logger.cpp
+++ src/arduino_current_logger/main_arduino_logger.cpp
@@ -2,29 +2,33 @@
 #include <Insomnia.h> //         https://github.com/chischte/insomnia-delay-library
 
 Insomnia status_print_delay;
-Insomnia current_clamp_reset_timeout(120000);
+// Insomnia current_clamp_reset_timeout(3000);
+Insomnia current_clamp_reset_timeout(3000);
 
-// RS PRO AC/DC CURRENT CLAMP:
-//
-
+// PIN DEFINITION -----------------------------------------------------------
+// INPUT:
 const byte CURRENT_CLAMP_IN = A0;
-const byte CLAMP_POWER_PIN = A4;
-const bool run_debug_plot_mode = true;
+
+// OUTPUT / POWER SUPPLY:
+const byte CLAMP_POWER_PIN1 = A3;
+const byte CLAMP_POWER_PIN2 = A4;
+
+// VARIOUS FUNCTIONS -----------------------------------------------------------
+
+void print_device_status() {
+  if (status_print_delay.delay_time_is_up(3000)) {
+    Serial.println("LOG;CURRENT_LOGGER_RUNNING;");
+  }
+}
 
 float get_amps_from_clamp() {
-  int analog_read = analogRead(CURRENT_CLAMP_IN);
-
-  // measured: 40 AMPS = ANALOG READ 89
-  float amps = float(analog_read) * 40.0 / 88.0;
+  // Measured: 40A = analog read 88
+  float amps = float(analogRead(CURRENT_CLAMP_IN)) * 40.0 / 88.0;
   return amps;
-}
-void run_plot_mode() {
-  Serial.println(get_amps_from_clamp(), 2);
-  delay(100);
 }
 
 // MANAGE CLAMP POWER SUPPLY ---------------------------------------------------
-// The camp too has o be switch off and on every few minutes to prevent
+// The clamp tool has to be switched off and on every few minutes to prevent
 // auto power off
 // Two output pins are used to draw less current per pin
 // Ports command are used to switch the pins simultaneously
@@ -34,6 +38,7 @@ void run_plot_mode() {
 void switch_clamp_power_on() {
   PORTB = PORTB | 0b00011000; // set bit 3 and 4 high, leave rest alone
 }
+
 void switch_clamp_power_off() {
   PORTB = PORTB & 0b11100111; // clear bit 3 and 4, leave rest alone
 }
@@ -41,56 +46,7 @@ void switch_clamp_power_off() {
 void reset_current_clamp() {
   switch_clamp_power_off();
   delay(1000);
-  digitalWrite(CLAMP_POWER_PIN, HIGH);
   switch_clamp_power_on();
-}
-// -----------------------------------------------------------------------------
-
-void print_device_status() {
-  if (status_print_delay.delay_time_is_up(3000)) {
-    Serial.println("LOG;CURRENT_LOGGER_RUNNING;");
-  }
-}
-
-// Start monitoring when current exceeds threshold
-// Log max current ONCE when current falls below threshold
-// Reset max current
-// Wait until current exceds threshold again
-void run_log_max_mode() {
-  static float max_current = -1.0; // -1 means not activated
-  float cycle_start_threshold = 20.0; // [A]
-  float cycle_stop_threshold = 15.0; // [A]
-  static bool current_is_over_threshold = false;
-
-  float current_current = get_amps_from_clamp();
-
-  if (current_current > cycle_start_threshold) {
-    current_is_over_threshold = true;
-  }
-  if (current_current < cycle_stop_threshold) {
-    current_is_over_threshold = false;
-  }
-
-  if (current_is_over_threshold) {
-    if (current_current > max_current) {
-      max_current = current_current;
-    }
-  }
-
-  if (!current_is_over_threshold) {
-    if (max_current > 0) {
-      //log current
-      max_current = -1;
-    }
-  }
-
-  float current = random(2000, 5400) / 100.0;
-  String prefix = "LOG;CURRENT_MAX;";
-  String suffix = ";";
-
-  Serial.println(prefix + current + suffix); // [A]
-
-  print_device_status();
 }
 
 void monitor_current_clamp_timeout() {
@@ -100,24 +56,67 @@ void monitor_current_clamp_timeout() {
   }
 }
 
+// LOG CURRENT -----------------------------------------------------------------
+// Start monitoring when current exceeds threshold
+// Log max current once when current falls below threshold
+// Reset max current
+
+void log_current(float current) {
+  String prefix = "LOG;CURRENT_MAX;";
+  String suffix = ";";
+  Serial.println(prefix + current + suffix); // [A]
+}
+
+bool current_is_over_threshold(float current) {
+  const float cycle_start_threshold = 15.0; // [A]
+  const float cycle_stop_threshold = 10.0; //  [A]
+  static bool current_is_over_threshold = false;
+
+  if (current > cycle_start_threshold) {
+    current_is_over_threshold = true;
+  }
+
+  if (current < cycle_stop_threshold) {
+    current_is_over_threshold = false;
+  }
+  return current_is_over_threshold;
+}
+
+void log_max_current() {
+  static float max_current = 0;
+
+  float current = get_amps_from_clamp();
+
+  if (current_is_over_threshold(current)) {
+    if (current > max_current) {
+      max_current = current;
+    }
+  } else if (max_current > 0) {
+    log_current(max_current);
+    max_current = 0; // reset max current.
+  }
+}
+
 // SETUP -----------------------------------------------------------------------
 
 void setup() {
-  pinMode(CLAMP_POWER_PIN, OUTPUT);
-  digitalWrite(CLAMP_POWER_PIN, HIGH);
+  pinMode(CLAMP_POWER_PIN1, OUTPUT);
+  pinMode(CLAMP_POWER_PIN2, OUTPUT);
+  switch_clamp_power_on();
+
   Serial.begin(115200);
-  Serial.println("EXIT SETUP"); // [A]
+  Serial.println("EXIT SETUP");
 }
 
 // LOOP ------------------------------------------------------------------------
 
 void loop() {
 
-  if (run_debug_plot_mode) {
-    run_plot_mode();
-  } else {
-    run_log_max_mode();
-  }
+  log_max_current();
+
+  print_device_status();
 
   monitor_current_clamp_timeout();
+
+  // Serial.println(get_amps_from_clamp(), 2); // For debug and calibration
 }
